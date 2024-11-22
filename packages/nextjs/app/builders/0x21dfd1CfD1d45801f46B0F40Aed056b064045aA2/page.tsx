586ad5541781cdfa4648bb437c6cc9e52aa5f826
Abolare Roheemah <roheemahabo@gmail.com>
2024-11-23 00:52:34 +0100

    requested changes from review

--- packages/nextjs/app/builders/0x21dfd1CfD1d45801f46B0F40Aed056b064045aA2/page.tsx
+++ packages/nextjs/app/builders/0x21dfd1CfD1d45801f46B0F40Aed056b064045aA2/page.tsx
@@ -1,9 +1,7 @@
-"use client";
-
-import React from "react";
 import Link from "next/link";
+import type { NextPage } from "next";
 
-export default function RoheemahBuilderPage() {
+const RoheemahBuilderPage: NextPage = () => {
   return (
     <div className="min-h-screen flex flex-col md:flex-row items-top justify-start gap-16 px-4 md:px-20">
       <div className="flex-1">
@@ -19,71 +17,43 @@ export default function RoheemahBuilderPage() {
                 <p className="text-md text-[#9B9EA1]">Frontend & Blockchain Dev</p>
               </div>
               <div className="mt-6 text-[#9B9EA1]">
-                <div className="flex items-center justify-start gap-4 mb-4">
-                  {/* <p><MailOutlined className='text-[#6AECD9]' /></p> */}
-                  {/* <img src="/link.svg" alt="" width={24} height={24} /> */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#6AECD9"
-                    className="size-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
-                    />
-                  </svg>
-                  <span>abolareroheemah@gmail.com</span>
-                </div>
-                <div className="flex items-center justify-start gap-4 mb-4">
-                  {/* <img src="/link.svg" alt="" width={24} height={24} /> */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#6AECD9"
-                    className="size-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
-                    />
-                  </svg>
-                  <Link href="https://www.github.com/AbolareRoheemah">www.github.com/AbolareRoheemah</Link>
-                </div>
-                <div className="flex items-center justify-start gap-4 mb-4">
-                  {/* <img src="/link.svg" alt="" width={24} height={24} /> */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#6AECD9"
-                    className="size-6"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
-                    />
-                  </svg>
-                  <Link href="https://rhorheeymarh.vercel.app">https://rhorheeymarh.vercel.app</Link>
-                </div>
+                {[
+                  { icon: "link", text: "abolareroheemah@gmail.com" },
+                  {
+                    icon: "link",
+                    text: "www.github.com/AbolareRoheemah",
+                    href: "https://www.github.com/AbolareRoheemah",
+                  },
+                  { icon: "link", text: "https://rhorheeymarh.vercel.app", href: "https://rhorheeymarh.vercel.app" },
+                ].map(({ text, href }) => (
+                  <div key={text} className="flex items-center justify-start gap-4 mb-4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="#6AECD9"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                      />
+                    </svg>
+                    {href ? <Link href={href}>{text}</Link> : <span>{text}</span>}
+                  </div>
+                ))}
               </div>
               <div className="flex items-center justify-between flex-wrap gap-2">
-                <p className="bg-[#9BF9F3] text-[#000] text-sm px-2 py-[1px] basis-2/12 rounded-full">HTML/CSS</p>
-                <p className="bg-[#9BF9F3] text-[#000] text-sm px-2 py-[1px] basis-2/12 rounded-full">JS</p>
-                <p className="bg-[#9BF9F3] text-[#000] text-sm px-2 py-[1px] basis-2/12 rounded-full">REACT</p>
-                <p className="bg-[#9BF9F3] text-[#000] text-sm px-2 py-[1px] basis-2/12 rounded-full">NEXT</p>
-                <p className="bg-[#9BF9F3] text-[#000] text-sm px-2 py-[1px] basis-2/12 rounded-full">VUE</p>
-                <p className="bg-[#9BF9F3] text-[#000] text-sm px-2 py-[1px] basis-2/12 rounded-full">NUXT</p>
-                <p className="bg-[#9BF9F3] text-[#000] text-sm px-2 py-[1px] basis-2/12 rounded-full">SOLIDITY</p>
-                <p className="bg-[#9BF9F3] text-[#000] text-sm px-2 py-[1px] basis-2/12 rounded-full">RUST</p>
+                {["HTML/CSS", "JS", "REACT", "NEXT", "VUE", "NUXT", "SOLIDITY", "RUST"].map(skill => (
+                  <p
+                    key={skill}
+                    className="bg-[#9BF9F3] text-[#000] text-center text-sm px-2 py-[1px] basis-2/12 rounded-full"
+                  >
+                    {skill}
+                  </p>
+                ))}
               </div>
               <a
                 href="https://docs.google.com/document/d/1iLtYWAaMytZ2K9NEQ3f8LVEUUOVBfcGFhkmYYE1tPdI/edit?usp=sharing"
@@ -126,22 +96,25 @@ export default function RoheemahBuilderPage() {
                             </div> */}
             </div>
             <div className="py-10 px-8 bg-[#1A1E23] rounded-lg md:rounded-full">
-              <div className="flex items-center justify-start md:items-center md:justify-center gap-4 mt-8">
-                <p className="text-[#6AECD9] text-[50px] font-medium">4</p>
-                <p className=" text-md font-bold text-[#9B9EA1]">Programming Languages</p>
-              </div>
-              <div className="flex items-center justify-start md:items-center md:justify-center gap-4 mt-8">
-                <p className="text-[#6AECD9] text-[50px] font-medium">6</p>
-                <p className=" text-md font-bold text-[#9B9EA1]">Development Tools</p>
-              </div>
-              <div className="flex items-center justify-start md:items-center md:justify-center gap-4 mt-8">
-                <p className="text-[#6AECD9] text-[50px] font-medium">3+</p>
-                <p className=" text-md font-bold text-[#9B9EA1]">Years of Experience</p>
-              </div>
+              {[
+                { number: "4", text: "Programming Languages" },
+                { number: "6", text: "Development Tools" },
+                { number: "3+", text: "Years of Experience" },
+              ].map(item => (
+                <div
+                  key={item.text}
+                  className="flex items-center justify-start md:items-center md:justify-center gap-4 mt-8"
+                >
+                  <p className="text-[#6AECD9] text-[50px] font-medium">{item.number}</p>
+                  <p className="text-md font-bold text-[#9B9EA1]">{item.text}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default RoheemahBuilderPage;
