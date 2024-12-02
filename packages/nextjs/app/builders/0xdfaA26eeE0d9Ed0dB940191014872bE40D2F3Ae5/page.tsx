c38707dcd98786820566b916c0de9c88d3e9b2e2
Asante-ntiro <asante.ntiro@gmail.com>
2024-12-03 02:36:22 +0300

    tweaking builder page

--- packages/nextjs/app/builders/0xdfaA26eeE0d9Ed0dB940191014872bE40D2F3Ae5/page.tsx
+++ packages/nextjs/app/builders/0xdfaA26eeE0d9Ed0dB940191014872bE40D2F3Ae5/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { NextPage } from "next";
 import { FaEnvelope, FaGithub, FaLinkedinIn } from "react-icons/fa6";
 import { Address } from "~~/components/scaffold-eth";
@@ -5,9 +6,8 @@ import { Address } from "~~/components/scaffold-eth";
 const styles = {
   card: "max-w-4xl mx-auto p-12 bg-base-100 rounded-2xl shadow-xl border border-base-300 dark:bg-base-200 dark:border-base-400",
   profileSection: "flex gap-4 flex-col md:flex-row items-center md:items-start",
-  avatarContainer: "relative group",
-  avatar: "w-40 h-40 md:w-48 md:h-48 rounded-2xl shadow-lg transition-transform duration-300 group-hover:scale-105",
-  nameSection: "flex-1 text-center md:text-left space-y-2",
+  avatarContainer: "relative group flex justify-center items-center gap-2",
+  nameSection: "flex-1 text-center md:text-center space-y-2",
   name: "text-4xl font-extrabold text-base-content dark:text-white bg-gradient-to-r from-primary to-secondary bg-clip-text",
   section: "mt-10",
   sectionTitle: "text-2xl font-extrabold mb-6 text-base-content dark:text-white flex items-center gap-2",
@@ -59,23 +59,25 @@ const AsanteNtiroBuilderPage: NextPage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-white to-gray-100 dark:from-gray-900 dark:to-gray-800 text-gray-900 dark:text-white py-16 px-4">
       <div className={styles.card}>
-        <div className="">
-          <div className={styles.avatarContainer}>
-            <img src={builderDetails.links.avatar} alt={builderDetails.name} className={styles.avatar} />
-          </div>
-          <div className={styles.nameSection}>
-            <h1 className={styles.name}>{builderDetails.name}</h1>
-            <div className={styles.addressContainer}>
-              <Address address={builderDetails.address} />
-            </div>
+        <div className="relative group flex justify-center items-center gap-2">
+          <Image
+            src={builderDetails.links.avatar}
+            className="w-40 h-40 md:w-48 md:h-48 rounded-2xl shadow-lg transition-transform duration-300 group-hover:scale-105"
+            alt="Gil Profile Picture"
+            width={256}
+            height={256}
+          />
+        </div>
+        <div className={styles.nameSection}>
+          <h1 className={styles.name}>{builderDetails.name}</h1>
+          <div className={styles.addressContainer}>
+            <Address address={builderDetails.address} />
           </div>
         </div>
 
         <div className={styles.divider} />
 
-        <h1 className="text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r dark:from-grey-600 dark:to-grey-600 ">
-          About Me
-        </h1>
+        <h1 className="flex-1 text-5xl text-center md:text-center space-y-2">About Me</h1>
         <h2 className={styles.sectionTitle}></h2>
         <p className={styles.bio}>{builderDetails.bio}</p>
 
