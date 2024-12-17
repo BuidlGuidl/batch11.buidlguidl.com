be9b9c80e094412c653bc6b28e9f5edfabfd5aa5
phipsae <philip.krause88@yahoo.de>
2024-12-17 10:16:08 +0100

    remove argument for deploying pixelCanva in deployment script

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -34,7 +34,6 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
 
   await deploy("PixelCanvas", {
     from: deployer,
-    args: [deployer], // Added deployer as an argument similar to BatchRegistry
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
